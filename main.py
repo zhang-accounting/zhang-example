11aa31a1635384e87db124804416b0840a856423
Kilerd Chan <blove694@gmail.com>
2024-05-06 16:38:22 +0800

    add budgt exmaple

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ COMMODITIES_SYMBOL = ["¥", "$"]
 
 COMPANY = 'GrandLordCompany'
 
-
-INCOMES = [f"{COMPANY}:Salary", "Remuneration", "CitiBank:Interest", "Publisher:DogeRandomHouse", "Publisher:TheZhangBookGroup", "Publisher:Scgolastic"]
+INCOMES = [f"{COMPANY}:Salary", "Remuneration", "CitiBank:Interest", "Publisher:DogeRandomHouse",
+           "Publisher:TheZhangBookGroup", "Publisher:Scgolastic"]
 ASSETS = ["Paypal", "WeChat", "CitiBank:9999"]
 LIABILITIES = ["CreditCard:Apple", "CreditCard:CitiBank:3423"]
-EXPENSES = ["PersonalDiet:Lunch", "PersonalDiet:Dinner", "Clothing", "Medical", "Sport", "Transportaion", f"{COMPANY}:Tax", "RemunerationTax"]
+EXPENSES = ["PersonalDiet:Lunch", "PersonalDiet:Dinner", "Clothing", "Medical", "Sport", "Transportaion",
+            f"{COMPANY}:Tax", "RemunerationTax"]
 EQUITIES = ["OpenBlancing"]
 
-
 EXPENSE_TYPE = [
-    [ # lunch, dinner
-        lambda: randint(0,2),
+    [  # lunch, dinner
+        lambda: randint(0, 2),
         ["Github Restaurant", "KFC", "Mcdonald", "隆江猪脚饭"],
         ["Eating Lunch", "Eating Dinner", "Fried Chicken"],
         [
             [
-                [*list(map(lambda item: f"Liabilities:{item}", LIABILITIES)), *list(map(lambda item: f"Assets:{item}", ASSETS))],
+                [*list(map(lambda item: f"Liabilities:{item}", LIABILITIES)),
+                 *list(map(lambda item: f"Assets:{item}", ASSETS))],
                 lambda: f"{randint(10, 30) * -1} {COMMODITIES[0]}"
             ],
             [
@@ -33,9 +34,9 @@ EXPENSE_TYPE = [
                 lambda: ""
             ]
         ]
-     ],
-     [ # cloth
-        lambda: random.choice([0,0,0,1]),
+    ],
+    [  # cloth
+        lambda: random.choice([0, 0, 0, 1]),
         ["H & M", "UNIQLO", "Nike", "Adidas", "Vans"],
         ["T-shirt", "shoes", "jeans"],
         [
@@ -48,14 +49,16 @@ EXPENSE_TYPE = [
                 lambda: ""
             ],
         ]
-     ],
-     [ # remuneration
-        lambda: random.choice([0,1]),
+    ],
+    [  # remuneration
+        lambda: random.choice([0, 1]),
         ["Doge Random House", "The Zhang Book Group", "Scgolastic"],
-        ["Remuneration of <How to learn Zhang>", "Annual Remuneration", "Remuneration of <Good Good study, Day Day Up>"],
+        ["Remuneration of <How to learn Zhang>", "Annual Remuneration",
+         "Remuneration of <Good Good study, Day Day Up>"],
         [
             [
-                list(map(lambda item: f"Income:{item}", ["Publisher:DogeRandomHouse", "Publisher:TheZhangBookGroup", "Publisher:Scgolastic"])),
+                list(map(lambda item: f"Income:{item}",
+                         ["Publisher:DogeRandomHouse", "Publisher:TheZhangBookGroup", "Publisher:Scgolastic"])),
                 lambda: f"{randint(10, 50) * -1} {COMMODITIES[0]}"
             ],
             [
@@ -67,11 +70,26 @@ EXPENSE_TYPE = [
                 lambda: f"{randint(2, 5)} {COMMODITIES[0]}"
             ],
         ]
-     ]
     ]
+]
+BUDGETS = [
+    ["food", "Take out Food", "Daily Necessary", 500],
+    ["shopping", "Shopping", "Daily Necessary", 300],
+    ["habit", "Habit", "Spiritual Needs", 300]
+]
+
+BUDGETS_MAPPING ={
+    "PersonalDiet:Lunch": "food",
+    "PersonalDiet:Dinner": "food",
+    "Clothing": "shopping",
+    "Sport": "habit"
+}
+
 
 def random_in_list(l):
     return random.choice(l)
+
+
 class Cli:
 
     def run(self):
@@ -88,65 +106,80 @@ class Cli:
             content.append(f"{beginning_format} commodity {commodity}")
             content.append("  precision: 2")
             content.append(f"  prefix: \"{COMMODITIES_SYMBOL[idx]}\"")
-        
+
         content.append("\n")
 
         # open income account
         comma_commodities = ", ".join(COMMODITIES)
         for income in INCOMES:
             content.append(f"{beginning_format} open Income:{income} {comma_commodities}")
-        
+
         content.append("\n")
 
         # open assets account
         for account in ASSETS:
             content.append(f"{beginning_format} open Assets:{account} {comma_commodities}")
 
-            
         # open liability account
         content.append("\n")
         for account in LIABILITIES:
             content.append(f"{beginning_format} open Liabilities:{account} {comma_commodities}")
-        
+
         # open expense account
         content.append("\n")
         for account in EXPENSES:
             content.append(f"{beginning_format} open Expenses:{account} {comma_commodities}")
-        
+            if account in BUDGETS_MAPPING:
+                content.append(f"  budget: {BUDGETS_MAPPING[account]}")
+
         # open expense account
         content.append("\n")
         for account in EQUITIES:
             content.append(f"{beginning_format} open Equity:{account} {comma_commodities}")
-        
 
-        
         now = arrow.now()
 
-        #balance all assets account:
+
+        # balance all assets account:
         content.append("\n")
         target_month = now.shift(months=-3)
-        beginning_of_zhang = arrow.Arrow(target_month.year, target_month.month, 1, 1,1,1).shift(days=-1).format("YYYY-MM-DD HH:mm:ss")
-        
+        beginning_of_zhang = arrow.Arrow(target_month.year, target_month.month, 1, 1, 1, 1).shift(days=-1).format(
+            "YYYY-MM-DD HH:mm:ss")
+
         for account in ASSETS:
             amount = random.randint(130000, 170000)
-            content.append(f"{beginning_of_zhang} balance Assets:{account} {amount} {COMMODITIES[0]} with pad Equity:{EQUITIES[0]}")
+            content.append(
+                f"{beginning_of_zhang} balance Assets:{account} {amount} {COMMODITIES[0]} with pad Equity:{EQUITIES[0]}")
 
+        # init budget
+
+        for budget in BUDGETS:
+            content.append(f"{beginning_of_zhang} budget {budget[0]} CNY")
+            content.append(f"  alias: \"{budget[1]}\"")
+            content.append(f"  category: \"{budget[2]}\"")
+            content.append(f"\n")
 
 
         # loop month
         content.append("\n")
-        for month_offset in [-3,-2,-1, 0]:
+        for month_offset in [-3, -2, -1, 0]:
             content.append("\n")
             target_month = now.shift(months=month_offset)
+            beginning_of_target_month = arrow.Arrow(target_month.year, target_month.month, 1, 1, 1, 1).format(
+                "YYYY-MM-DD")
+            # add budget amount
+            for budget in BUDGETS:
+                content.append(f"{beginning_of_target_month}  budget-add {budget[0]} {budget[3]} CNY")
 
             # loop month: salary at month first day
             salary_income = random.randint(2000, 3000)
             salary_tax = random.randint(100, 400)
             salary_asset = salary_income - salary_tax
             implicit_tax = f"{salary_tax} {COMMODITIES[0]}" if random.choice([True, False]) else ""
-            time_of_salary = arrow.Arrow(target_month.year, target_month.month, 1, 8,0,0).format("YYYY-MM-DD HH:mm:ss")
+            time_of_salary = arrow.Arrow(target_month.year, target_month.month, 1, 8, 0, 0).format(
+                "YYYY-MM-DD HH:mm:ss")
             content.append(f"{time_of_salary} \"{COMPANY}\" \"Salary\"")
-            content.append(f"  Income:{COMPANY}:Salary {-1*salary_income} {COMMODITIES[0]}")
+            content.append(f"  Income:{COMPANY}:Salary {-1 * salary_income} {COMMODITIES[0]}")
             content.append(f"  Assets:CitiBank:9999 {salary_asset} {COMMODITIES[0]}")
             content.append(f"  Expenses:{COMPANY}:Tax {implicit_tax}")
 
@@ -158,19 +191,20 @@ class Cli:
                     continue
                 for [loop_executor, payee, narration, postings] in EXPENSE_TYPE:
                     for _loop_time in range(0, loop_executor()):
-                        target_time = arrow.Arrow(target_month.year, target_month.month, day, random.randint(0, 23),random.randint(0, 59),random.randint(0, 59)).format("YYYY-MM-DD HH:mm:ss")
+                        target_time = arrow.Arrow(target_month.year, target_month.month, day, random.randint(0, 23),
+                                                  random.randint(0, 59), random.randint(0, 59)).format(
+                            "YYYY-MM-DD HH:mm:ss")
                         content.append(f"{target_time} \"{random_in_list(payee)}\" \"{random_in_list(narration)}\"")
                         for [posting, amount_executor] in postings:
                             content.append(f"  {random_in_list(posting)} {amount_executor()}")
-                        
+
                         content.append("\n")
 
-
-            
         # loop month: income for remuneration, interest
         # balance at end of month
         pass
         print("\n".join(content))
+
 
 if __name__ == '__main__':
     fire.Fire(Cli)
